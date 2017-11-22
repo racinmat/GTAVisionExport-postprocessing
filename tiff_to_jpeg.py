9f1c84135c7782dd2e70838506070b2991a74d5d
racinmat <racinmat@fel.cvut.cz>
2017-11-22 16:07:08 +0100

    dded original postprocessing script.

--- tiff_to_jpeg.py
+++ tiff_to_jpeg.py
@@ -6,18 +6,26 @@ if __name__ == '__main__':
     in_directory = './../output'
     out_directory = './../Mask_RCNN/GTA-images'
     files = [
-        'info10.tiff',
-        'info11.tiff',
-        'info17.tiff',
-        'info18.tiff',
-        'info50.tiff',
-        'info51.tiff',
+        'info-2017-11-19--23-21-03.tiff',
+        'info-2017-11-19--23-21-11.tiff',
+        'info-2017-11-19--23-21-31.tiff',
+        'info-2017-11-19--23-21-25.tiff',
+        'info-2017-11-19--23-21-29.tiff',
+        'info-2017-11-19--23-20-42.tiff',
+        'info-2017-11-19--23-20-05.tiff',
+        'info-2017-11-19--23-20-11.tiff',
+        'info-2017-11-19--23-19-12.tiff',
+        'info-2017-11-19--23-19-19.tiff',
+    ]
+    frames = [
+        0, 1, 2, 3
     ]
     for name in files:
         base_name = os.path.splitext(name)[0]
-        im = Image.open(os.path.join(in_directory, name))
-        im.seek(0)  # 1.frame of tiff
-        im = im.convert(mode="RGB")
-        print("Generating jpeg for {}".format(name))
-        outfile = os.path.join(out_directory, base_name) + ".jpg"
-        im.save(outfile)
+        for frame in frames:
+            im = Image.open(os.path.join(in_directory, name))
+            im.seek(frame)
+            im = im.convert(mode="RGB")
+            print("Generating jpeg for {}".format(name))
+            outfile = os.path.join(out_directory, base_name) + "-{}.jpg".format(frame)
+            im.save(outfile)
