3a3b688865b1d3ffec29c28728b1446fd28e999b
racinmat <racinmat@fel.cvut.cz>
2017-11-26 14:15:49 +0100

    Parallel transforming images.

--- batch_tiff_to_jpeg.py
+++ batch_tiff_to_jpeg.py
@@ -1,9 +1,27 @@
 import glob
 import os
-
+from joblib import Parallel, delayed
 from PIL import Image
 
+
+def get_base_name(name):
+    return os.path.basename(os.path.splitext(name)[0])
+
+
+def tiff_to_jpg(in_directory, out_directory, base_name, name, frame):
+    outfile = os.path.join(out_directory, base_name) + "-{}.jpg".format(frame)
+    if os.path.exists(outfile):
+        return
+
+    im = Image.open(os.path.join(in_directory, name))
+    im.seek(frame)
+    im = im.convert(mode="RGB")
+    print("Generating jpeg for {}".format(name))
+    im.save(outfile)
+
+
 if __name__ == '__main__':
+    workers = 4
     in_directory = 'D:\\GTAV_extraction_output'
     # in_directory = 'D:\\projekty\\GTA-V-extractors\\output'
     out_directory = 'D:\\GTAV_extraction_output\\rgb-jpeg'
@@ -12,15 +30,12 @@ if __name__ == '__main__':
     frames = [
         0
     ]
-    for name in glob.glob(os.path.join(in_directory, pattern)):
-        base_name = os.path.basename(os.path.splitext(name)[0])
-        for frame in frames:
-            outfile = os.path.join(out_directory, base_name) + "-{}.jpg".format(frame)
-            if os.path.exists(outfile):
-                continue
 
-            im = Image.open(os.path.join(in_directory, name))
-            im.seek(frame)
-            im = im.convert(mode="RGB")
-            print("Generating jpeg for {}".format(name))
-            im.save(outfile)
+    Parallel(n_jobs=workers)(delayed(tiff_to_jpg)
+                             (in_directory, out_directory, get_base_name(name), name, frame) for frame in frames
+                             for name in glob.glob(os.path.join(in_directory, pattern)))
+
+    # for name in glob.glob(os.path.join(in_directory, pattern)):
+    #     base_name = get_base_name(name)
+    #     for frame in frames:
+    #         tiff_to_jpg(in_directory, out_directory, base_name, frame)
