fixed 2d bbox calculation

gta_math.py
@@ -462,9 +462,9 @@ def calculate_2d_bbox(row, view_matrix, proj_matrix, width, height):
     projected /= projected[3, :]
     bbox_3d = projected.T[:, 0:3]
 
-    bbox_2d_points = bbox_3d[:, 0:2]
-    is_3d_bbox_partially_outside = (bbox_2d_points < -1).any() or (bbox_2d_points > 1).any()
-    bbox_2d_points = bbox_2d_points[((bbox_2d_points <= 1) & (bbox_2d_points >= -1)).all(axis=1)]
+    bbox_2d_points_ndc = bbox_3d[:, 0:2]
+    is_3d_bbox_partially_outside = (bbox_2d_points_ndc < -1).any() or (bbox_2d_points_ndc > 1).any()
+    bbox_2d_points_ndc = bbox_2d_points_ndc[((bbox_2d_points_ndc <= 1) & (bbox_2d_points_ndc >= -1)).all(axis=1)]
     if is_3d_bbox_partially_outside:
         bbox_2d = model_coords_to_pixel(pos, rot, points_3dbbox, view_matrix, proj_matrix, width, height).T
         # now we need to compute intersections between end of image and points
@@ -498,14 +498,15 @@ def calculate_2d_bbox(row, view_matrix, proj_matrix, width, height):
                     l2 = Line(Point(border[0][0], border[0][1]), Point(border[1][0], border[1][1]))
                     x, y = np.array(next(iter(l1.intersect(l2))), dtype=np.float32)
                     ndc_y, ndc_x = pixel_to_ndc((y, x), (height, width))
-                    bbox_2d_points = np.vstack((bbox_2d_points, [ndc_x, ndc_y]))
+                    bbox_2d_points_ndc = np.vstack((bbox_2d_points_ndc, [ndc_x, ndc_y]))
 
-    bbox_2d = np.array([
-        [bbox_2d_points[:, 0].max(), -bbox_2d_points[:, 1].min()],
-        [bbox_2d_points[:, 0].min(), -bbox_2d_points[:, 1].max()],
+    # because of NDC, this 2d bbox extraction looks so weird
+    bbox_2d_ndc = np.array([
+        [bbox_2d_points_ndc[:, 0].max(), -bbox_2d_points_ndc[:, 1].min()],
+        [bbox_2d_points_ndc[:, 0].min(), -bbox_2d_points_ndc[:, 1].max()],
     ])
     # rescale from [-1, 1] to [0, 1]
-    bbox_2d = (bbox_2d / 2) + 0.5
+    bbox_2d = (bbox_2d_ndc / 2) + 0.5
     return bbox_2d.tolist()
 
 

kitti.py
@@ -1,4 +1,22 @@
-def image_gta_to_kitti():
+import numpy as np
+
+
+def get_kitti_vfov():
+    # gets kitti color camera vertical field of view, calculated from inspecting-kitti-calibration.ipynb
+    return 54.63
+
+
+def get_kitti_hfov():
+    # gets kitti color camera horizontal field of view, calculated from inspecting-kitti-calibration.ipynb
+    return 119.64
+
+
+def image_gta_to_kitti(data):
+    d = (data['width']/2) / np.sin(np.radians(data['fov']/2))
+    kitti_width = d * np.sin(np.radians(get_kitti_hfov()))
+
+    d = (data['height']/2) / np.sin(np.radians(data['fov']/2))
+    kitti_height = d * np.sin(np.radians(get_kitti_vfov()))
     pass  # todo: implement
 
 

toyota.py
@@ -55,17 +55,17 @@ def vehicle_type_gta_to_toyota(gta_type):
     """
     # todo: výhledově přidat sběr modelů
     mapping = {
-        'Compacts': 'VEHICLE_CITY_CAR',
-        'Sedans': 'VEHICLE_PASSENGER_CAR',
+        'Compacts': 'CITY_CAR',
+        'Sedans': 'PASSENGER_CAR',
         'SUVs': 'SUV',
-        'Coupes': 'VEHICLE_PASSENGER_CAR',
-        'Muscle': 'VEHICLE_PASSENGER_CAR',
-        'SportsClassics': 'VEHICLE_PASSENGER_CAR',
-        'Sports': 'VEHICLE_PASSENGER_CAR',
-        'Super': 'VEHICLE_PASSENGER_CAR',
+        'Coupes': 'PASSENGER_CAR',
+        'Muscle': 'PASSENGER_CAR',
+        'SportsClassics': 'PASSENGER_CAR',
+        'Sports': 'PASSENGER_CAR',
+        'Super': 'PASSENGER_CAR',
         'Motorcycles': 'OTHER',
         'OffRoad': 'SUV',
-        'Industrial': 'VEHICLE_TRUCK',
+        'Industrial': 'TRUCK',
         'Utility': 'OTHER',
         'Vans': 'VAN',
         'Cycles': 'OTHER',
@@ -168,9 +168,10 @@ def out_of_image_2dbbox_ratio(entity, view_matrix, proj_matrix, width, height):
     point_homo = np.array(
         [points_3dbbox[:, 0], points_3dbbox[:, 1], points_3dbbox[:, 2], np.ones_like(points_3dbbox[:, 0])])
     bbox_3d = model_coords_to_pixel(entity['pos'], entity['rot'], point_homo.T, view_matrix, proj_matrix, width, height)
+    # this bbox extraction is in pixel coordinates, that is why it looks different than in gta_math script
     bbox_2d = np.array([
-        [bbox_3d[:, 0].max(), -bbox_3d[:, 1].min()],
-        [bbox_3d[:, 0].min(), -bbox_3d[:, 1].max()],
+        [bbox_3d[0, :].max(), bbox_3d[0, :].max()],
+        [bbox_3d[1, :].min(), bbox_3d[1, :].min()],
     ])
     image = np.array([
         [width, height],
@@ -183,7 +184,7 @@ def out_of_image_2dbbox_ratio(entity, view_matrix, proj_matrix, width, height):
     in_image = get_rectangles_overlap(bbox_2d, image)
     in_image_volume = get_rectangle_volume(in_image)
     whole_volume = get_rectangle_volume(bbox_2d)
-    return in_image_volume / whole_volume
+    return (whole_volume - in_image_volume) / whole_volume
 
 
 def get_my_car_position_and_rotation(cam_pos, cam_rot, cam_rel_pos, cam_rel_rot):